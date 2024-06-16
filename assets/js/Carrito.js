2468e6d618069fde48b2a30fbb7a1579e64eb9e0
Gus <ghuachaca25@gmail.com>
2024-06-16 17:40:11 -0500

    agregando mas Productos

--- assets/js/Carrito.js
+++ assets/js/Carrito.js
@@ -1,27 +1,7 @@
- // Variables para el carrito
- let carrito = [];
- let totalPrecio = 0;
- const badgeCarrito = document.getElementById('carritoCantidad');
+document.addEventListener('DOMContentLoaded', function() {
+    const carritoCantidad = document.getElementById('carritoCantidad');
+    let cantidadProductos = localStorage.getItem('cantidadProductos') ? parseInt(localStorage.getItem('cantidadProductos')) : 0;
+    carritoCantidad.textContent = cantidadProductos;
 
- // Función para agregar al carrito
- function agregarAlCarrito(nombre, precio) {
-     carrito.push({ nombre, precio });
-     totalPrecio += precio;
-     badgeCarrito.innerText = carrito.length;
-     alert(`Producto ${nombre} agregado al carrito. Total: $${totalPrecio}`);
- }
-
- // Event listener para botones de agregar al carrito
- const botonesAgregar = document.querySelectorAll('.agregar-carrito');
- botonesAgregar.forEach(boton => {
-     boton.addEventListener('click', function() {
-         const nombre = this.getAttribute('data-nombre');
-         const precio = parseFloat(this.getAttribute('data-precio'));
-         agregarAlCarrito(nombre, precio);
-     });
- });
-
- document.getElementById('loginLink').addEventListener('click', function(event) {
- event.preventDefault(); // Evita el comportamiento predeterminado del enlace
- window.location.href = 'Login.html'; // Redirige a la página de login
-});  
+    // Este código no debe escuchar a los botones agregar-carrito aquí, ya que es para la página del carrito solamente
+});
